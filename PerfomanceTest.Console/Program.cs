b5f884b61cacde5257ba3d91bc53fe08a6c79177
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-01 08:21:37 +0330

    Add project files.

---
+++ PerfomanceTest.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PerfomanceTest.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
